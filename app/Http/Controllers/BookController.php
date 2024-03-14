a93f4b3b90e41f5251ec32fe179554f39836aee8
gaonkarsahil <sahilgaonkar24@gmail.com>
2024-03-14 21:16:49 +0530

    added controller logic

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,61 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Book;
 
 class BookController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Display a listing of all the books.
      */
     public function index()
     {
-        //
+        return Book::all();
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * Store a newly created book in library.
      */
     public function store(Request $request)
     {
-        //
+        return Book::create($request->all());
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * Display the specified book.
      */
-    public function show($id)
+    public function show(Book $book)
     {
-        //
+        return $book;
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * Update the specified book in library.
      */
-    public function update(Request $request, $id)
-    {
-        //
+    public function update(Request $request, Book $book)
+    {        
+        $book->update($request->all());
+        return $book;
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * Remove the specified book from library.
      */
-    public function destroy($id)
+    public function destroy(Book $book)
     {
-        //
+        $book->delete();
+        return response()->json(['message' => 'Book deleted successfully']);
     }
 }
